7d53d8801634e89e1b2d4ff4796aaff1574c886f
CharlesKibet <charlesbiegon973@gmail.com>
2025-02-17 19:36:08 +0300

    markdown
    Added a readme file for code documentation

---
+++ README.md
@@ -0,0 +1,11 @@
+# Optimizing web application performance
+
+## Introduction
+
+This document provides a brief overview of the performance optimization techniques that can be used to improve the performance of a web application.
+
+1. [Using Flask and Redis to optimize web application performance]
+   (https://medium.com/@fahadnujaimalsaedi/using-flask-and-redis-to-optimize-web-application-performance-34a8ae750097)
+
+
+
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Using flask and redis to optimize web application perfomance
 import datetime
 from flask import Flask, request, jsonify
 from flask_migrate import Migrate
@@ -18,28 +19,21 @@ app.config['CACHE_REDIS_DB'] = 0
 db.init_app(app)
 cache = Cache()
 cache.init_app(app)
-migrate = Migrate(app=app,db=db)
-# Initializing Redis
+migrate = Migrate(app=app, db=db)
+
+# Initializing Redis client
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 
 app.route('/items', methods=['POST'])
 
 
-def add_item():
-    # Getting the item name from the request bdy
-    item_name = request.json.get('name')
-
-    # Delete the cached response to invalidate the cache
-    cache.delete('items')
-    return jsonify({'message': 'Item added successfuly'})
-
-
 @app.route('/items', methods=['GET'])
-@cache.cached(timeout=60, key_prefix='items')
+# Caching this get items for 5 minutes
+@cache.cached(timeout=300, key_prefix='items')
 def get_items():
     # Check if the response is already cached
-    cached_response = redis.client.get('items')
+    cached_response = redis_client.get('items')
     if cached_response:
         return jsonify(cached_response)
 
